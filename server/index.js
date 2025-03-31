b8fd8273baa19c261c5946b7dda79931d7e9663c
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-01 02:57:20 +0530

    feat: Database connection

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import dotenv from "dotenv"
+import cors from 'cors'
+import connectDB from './config/connectDB.js'
+
+dotenv.config()
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Cache-Control",
+        "Expires",
+        "Pragma",
+      ],
+      credentials: true,
+    })
+  );
+
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+  
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server is now running on port ${PORT}`)
+    })
+})
