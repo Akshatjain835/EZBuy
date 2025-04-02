bdff7d4090e4165e4ff0515a70de541902a00bb6
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-03 02:56:45 +0530

    feat:added Admin components and pages page

--- client/src/components/admin-view/AdminLayout.jsx
+++ client/src/components/admin-view/AdminLayout.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import AdminSideBar from "./AdminHeader.jsx";
+import AdminHeader from "./AdminSidebar.jsx";
+import { useState } from "react";
 
-const AdminLayout = () => {
+function AdminLayout() {
+
+
   return (
-    <div>AdminLayout</div>
-  )
+    <div className="flex min-h-screen w-full">
+      {/* admin sidebar */}
+      <AdminSideBar  />
+      <div className="flex flex-1 flex-col">
+        {/* admin header */}
+        <AdminHeader  />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default AdminLayout
+export default AdminLayout;
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,31 +1,51 @@
-
 import AuthLayout from "@/components/auth/AuthLayout.jsx";
 import { createBrowserRouter } from "react-router-dom";
-import Login from "../pages/auth/Login.jsx"
-import Register from "../pages/auth/Register.jsx"
-
+import Login from "../pages/auth/Login.jsx";
+import Register from "../pages/auth/Register.jsx";
+import AdminLayout from "@/components/admin-view/AdminLayout.jsx";
+import AdminDashboard from "@/pages/admin-view/AdminDashboard.jsx";
+import AdminProducts from "@/pages/admin-view/AdminProducts.jsx";
+import AdminOrders from "@/pages/admin-view/AdminOrders.jsx";
+import AdminFeatures from "@/pages/admin-view/AdminFeatures.jsx";
 
 const appRouter = createBrowserRouter([
-  
+  {
+    path: "/auth",
+    element: <AuthLayout />,
 
-    {
-        path: "/auth",
-        element: <AuthLayout />,
-
-        children: [
-          { 
-            path: "login",
-             element: <Login />
-             },
-          { 
-            path: "register",
-             element: <Register /> 
-            },
-        ],
+    children: [
+      {
+        path: "login",
+        element: <Login />,
       },
- 
-
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },
+  {
+    path:"/admin",
+    element:<AdminLayout/>,
+    children: [
+        {
+          path: "dashboard",
+          element: <AdminDashboard />,
+        },
+        {
+          path: "products",
+          element: <AdminProducts />,
+        },
+        {
+            path: "orders",
+            element: <AdminOrders />,
+          },
+          {
+            path: "features",
+            element: <AdminFeatures />,
+          },
+      ],
+  }
 ]);
 
 export default appRouter;
-
