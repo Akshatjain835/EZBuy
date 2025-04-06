e233df7be565f1d962ac3c40b6980f557a61db2a
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-06 19:08:51 +0530

    feat: added fetch,update,edit and delete product controller

--- server/controllers/products.controller.js
+++ server/controllers/products.controller.js
@@ -1,5 +1,6 @@
 // controllers/productController.js
 
+import Product from "../models/products.model.js";
 import { imageUploadUtil } from "../utils/cloudinary.js";
 
 
@@ -68,6 +69,123 @@ export const addProductController = async (req, res) => {
     }
   };
   
+//fetch all products
+export const fetchAllProductsController = async (req, res) => {
+
+    try {
+
+      const listOfProducts = await Product.find({});
+
+      res.status(200).json({
+        success: true,
+        data: listOfProducts,
+
+
+      });
+
+    } catch (e) {
+
+      // console.log(e);
+      res.status(500).json({
+        success: false,
+        message: "Error occured",
+      });
+    }
+  };
+  
+
+  //edit a product
+export const editProductController = async (req, res) => {
+    try {
+    
+      const { id } = req.params;
+      // console.log(id)
+
+      const { image, title, description, category, brand, price, salePrice, totalStock} = req.body;
+  
+      let findProduct = await Product.findById(id);
+      // console.log(findProduct)
+
+      if (!findProduct){
+
+        return res.status(404).json({
+          success: false,
+          message: "Product not found",
+          error:true
+        });
+
+      }
+  
+      findProduct.title = title || findProduct.title;
+
+      findProduct.description = description || findProduct.description;
+
+      findProduct.category = category || findProduct.category;
+
+      findProduct.brand = brand || findProduct.brand;
+
+      findProduct.price = price === "" ? 0 : price || findProduct.price;
+
+      findProduct.salePrice =salePrice === "" ? 0 : salePrice || findProduct.salePrice;
+
+      findProduct.totalStock = totalStock || findProduct.totalStock;
+
+      findProduct.image = image || findProduct.image;
+  
+  
+      await findProduct.save();
+
+      res.status(200).json({
+        success: true,
+        data: findProduct,
+        error:false
+      });
+
+    } catch (e) {
+
+      // console.log(e);
+
+      res.status(500).json({
+        success: false,
+        message: "Error occured",
+      });
+    }
+  };
+  
+
+  //delete a product
+export const deleteProductController = async (req, res) => {
+
+  try {
+
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product){
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+
+      success: true,
+      message: "Product delete successfully",
+      error:false
+    });
+  } catch (e) {
+    // console.log(e);
+
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+      error:true
+    });
+  }
+};
+
   
 
 
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,12 +1,17 @@
 
 import { Router } from "express";
-import { handleImageUploadController } from "../controllers/products.controller.js";
+import { addProductController, deleteProductController, editProductController, fetchAllProductsController, handleImageUploadController } from "../controllers/products.controller.js";
 import { upload } from "../utils/cloudinary.js";
 
 
 const productRouter = Router();
 
 productRouter.post("/upload-image", upload.single("my_file"), handleImageUploadController);
+productRouter.post("/add",addProductController);
+productRouter.get("/get",fetchAllProductsController);
+productRouter.put("/edit/:id",editProductController);
+productRouter.delete("/delete/:id",deleteProductController);
+
 
 
 export default productRouter
