4137489b1eed0792cb01dc212d555c4d24df7c93
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-10 23:46:37 +0530

    feat: added address model

---
+++ server/models/address.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema(
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      address: {
+        type: String,
+        required: true
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      pincode: {
+        type: String,
+        required: true
+      },
+      phone: {
+        type: String,
+        required: true
+      },
+      notes: {
+        type: String,
+        default: ''
+      }
+    },
+    { timestamps: true }
+  );
+  
+  
+const Address=mongoose.model("Address",AddressSchema);
+export default Address;
