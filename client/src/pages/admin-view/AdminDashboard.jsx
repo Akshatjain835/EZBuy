0491c9b0e8b00d82c92a0a7e7a9f8112a128c515
Akshatjain835 <jainakshataj07@gmail.com>
2025-04-13 14:01:03 +0530

    feat : added admin dashboard page

--- client/src/pages/admin-view/AdminDashboard.jsx
+++ client/src/pages/admin-view/AdminDashboard.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import ProductImageUpload from '@/components/admin-view/ProductImageUpload'
+import { Button } from '@/components/ui/button'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 const AdminDashboard = () => {
+
+  const [imageFile, setImageFile] = useState(null);
+
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  // console.log(uploadedImageUrl, "uploadedImageUrl");
+  const [imageLoadingState, setImageLoadingState] = useState(false);
+
+  const dispatch = useDispatch();
+
+
+
   return (
-    <div>AdminDashboard</div>
+    <div>
+      <ProductImageUpload
+        imageFile={imageFile}
+        setImageFile={setImageFile}
+        uploadedImageUrl={uploadedImageUrl}
+        setUploadedImageUrl={setUploadedImageUrl}
+        setImageLoadingState={setImageLoadingState}
+        imageLoadingState={imageLoadingState}
+        isCustomStyling={true}
+        // isEditMode={currentEditedId !== null}
+      />
+      <Button onClick={handleUploadFeatureImage} className="mt-5 w-full">
+        Upload
+      </Button>
+      <div className="flex flex-col gap-4 mt-5">
+        {
+        featureImageList && featureImageList.length > 0
+          ? featureImageList.map((featureImgItem) => (
+              <div className="relative">
+                <img
+                  src={featureImgItem.image}
+                  className="w-full h-[300px] object-cover rounded-t-lg"
+                />
+              </div>
+            ))
+          : null
+          }
+      </div>
+    </div>
   )
 }
 
